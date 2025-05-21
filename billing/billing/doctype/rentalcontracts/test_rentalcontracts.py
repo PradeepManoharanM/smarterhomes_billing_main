08ec87c2a5694a9e573903a46d843fa50937c5b0
PradeepManoharanM <pradeep.m@frugalscientific.com>
2025-05-21 13:55:28 +0000

    Feat:Deleting Doctypes

--- billing/billing/doctype/rentalcontracts/test_rentalcontracts.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Frugal Scientific and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class Testrentalcontracts(FrappeTestCase):
-	pass
