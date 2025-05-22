34a991e978eff8c848e418ac9f7cd8fdc59bfadf
Pradeep <pradeep.m@frugalscientific.com>
2025-05-22 11:21:50 +0530

    Feat:New Doctype has created rentalcontracts

---
+++ billing/billing/doctype/rentalcontracts/test_rentalcontracts.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Frugal Scientific and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRentalContracts(FrappeTestCase):
+	pass
